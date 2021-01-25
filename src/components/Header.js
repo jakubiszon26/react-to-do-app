6732dd1c44a60cf21ce149f40afac18f2d11e833
jakubiszon26 <itcppprograming@gmail.com>
2021-01-25 20:15:12 +0100

    deleted create-react-app readme

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header ">
+            <h1 className="header-heading">
+                Let's see what you have to do...
+            </h1>
+        </div>
+    )
+}
