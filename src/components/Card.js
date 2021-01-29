872b56045296bc8ff7c8a10d5b09eea1e55f374b
jakubiszon26 <itcppprograming@gmail.com>
2021-01-29 22:15:32 +0100

    add task button style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-export default function Card({data}) {
+export default function Card({ data }) {
   return (
     <div className="card">
       <h2 className="card-title">{data.title}</h2>
       <ul className="card-ul">
-        {data.tasks.map(item => <li>
-          <h4>{item}</h4>
-        </li>)}
+        {data.tasks.map((item) => (
+          <li key={item.id}>
+            <h4>{item.title}</h4>
+          </li>
+        ))}
       </ul>
+      <div className="card-button">
+        <button className="add-task-button">Add task</button>
+      </div>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,7 +2,28 @@ const data = {
   cards: [
     {
       title: "To do",
-      tasks: ["task1", "task2", "task3"],
+      tasks: [
+        {
+          id: 1,
+          title: "task1",
+        },
+        {
+          id: 2,
+          title: "task1",
+        },
+        {
+          id: 3,
+          title: "task1",
+        },
+        {
+          id: 4,
+          title: "task1",
+        },
+        {
+          id: 5,
+          title: "task1",
+        },
+      ],
     },
   ],
 };
