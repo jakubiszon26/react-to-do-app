e0eebf278460d3fea2721edea0df055964c66dea
jakubiszon26 <itcppprograming@gmail.com>
2021-02-04 23:36:52 +0100

    api handle add new task to db

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ var con = mysql.createConnection({
   password: 'password',
   database: 'react_to_do',
 });
-con.connect()
+con.connect((err) => {
+  if(err) throw err;
+  console.log("connected to database")
+})
 app.get("/", (req, res) => {
   res.send("todoapp server");
 });
-app.post("/add", () => {
-  
+app.get("/add", (req, res) => {
+  con.query(`INSERT INTO tasks (text, cardId) VALUES (${req.query.text}, ${req.query.cardId} )`, (err, result) => {
+    if(err) throw err;
+    console.log(result)
+  });
 });
 app.listen(port, () => {
   console.log("server is listening on port: " + port);
